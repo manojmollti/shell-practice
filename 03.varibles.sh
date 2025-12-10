37402b084ee32e8ddb35251b2dbd10f39a794725
manoj <manojj5548@gmail.com>
2025-12-10 16:54:39 +0530

    varibles

---
+++ 03.varibles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1:: hi $person2 welcome"
+echo "$person2:: hello $person1"
+echo "$person1::had your meal $person2"
+echo "$person2::not yet $person1"
